7d2405e8c829b1ebfce61ace85409ae8e5a514c6
Ryo-15 <tomorrows.way15@gmail.com>
2020-10-16 11:53:01 +0900

    [finish]typing_js_lesson_4

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
 'use strict';
 
 {
+  // 単語の設定
+  const word = 'red';
+  // 何文字目を打っているか
+  let loc = 0;
+
+  // キー入力を取得
+  const target = document.getElementById('target');
+  target.textContent = word;
+
   document.addEventListener('keydown', e => {
-    const target = document.getElementById('target');
-    target.textContent = e.key;
+    // 文字入力の判定
+    if (e.key === word[loc]) {
+      loc++;
+
+      // 1: _ed
+      // 2: __d
+      // 3: ___
+
+    }
   });
 }
